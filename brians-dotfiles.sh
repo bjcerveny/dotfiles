38c31c4cdca840d9852c9219f4f32bc716a29441
Brian J Cerveny <brian.j.cerveny@intel.com>
2014-05-13 13:24:02 -0500

    Add upgrade function for dotfiles

--- brians-dotfiles.sh
+++ brians-dotfiles.sh
@@ -1,3 +1,7 @@
+function upgrade_brians_dotfiles() {
+  /usr/bin/env DOTFILES=$DOTFILES /bin/sh $DOTFILES/upgrade.sh
+}
+
 # Check for updates on initial load...
 if [ "$DISABLE_AUTO_UPDATE" != "true" ]; then
   /usr/bin/env DOTFILES=$DOTFILES DISABLE_UPDATE_PROMPT=$DISABLE_UPDATE_PROMPT zsh -f $DOTFILES/check_for_upgrade.sh
