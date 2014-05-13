0f5ade469a36812e25fcad24c85156a3fe8b5845
Brian J Cerveny <brian.j.cerveny@intel.com>
2014-05-13 10:22:33 -0500

    update script bugfix

--- upgrade.sh
+++ upgrade.sh
@@ -2,7 +2,8 @@ printf '\033[0;34m%s\033[0m\n' "Upgrading Brian's dotfiles"
 cd "$ZSH"
 if git pull --rebase --stat origin master
 then
-  printf '\033[0;34m%s\033[0m\n' "Brian\'s dotfiles have been updated and/or are at the current version."
+  printf '\033[0;34m%s\033[0m\n' "Brian's dotfiles have been updated and/or are at the current version."
+else
   printf '\033[0;31m%s\033[0m\n' 'There was an error updating. Try again later?'
 fi
 
