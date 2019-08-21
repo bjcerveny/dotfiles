787132dd06fda5fdd88269024f1711e6ee932cfe
Brian Cerveny <cerveny_brian@shure.com>
2019-08-21 15:06:57 -0500

    Update upgrader, change vim font

--- upgrade.sh
+++ upgrade.sh
@@ -1,9 +1,32 @@
-printf '\033[0;34m%s\033[0m\n' "Upgrading Brian's dotfiles"
+
+# Use colors, but only if connected to a terminal, and that terminal
+# supports them.
+if which tput >/dev/null 2>&1; then
+    ncolors=$(tput colors)
+fi
+if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+  RED="$(tput setaf 1)"
+  GREEN="$(tput setaf 2)"
+  YELLOW="$(tput setaf 3)"
+  BLUE="$(tput setaf 4)"
+  BOLD="$(tput bold)"
+  NORMAL="$(tput sgr0)"
+else
+  RED=""
+  GREEN=""
+  YELLOW=""
+  BLUE=""
+  BOLD=""
+  NORMAL=""
+fi
+
+printf "${BLUE}%s${NORMAL}\n" "Updating Oh My Zsh"
 cd "$DOTFILES"
 if git pull --rebase --stat origin master
 then
-  printf '\033[0;34m%s\033[0m\n' "Brian's dotfiles have been updated and/or are at the current version."
+  printf '%s' "$GREEN"
+  printf '%s\n' '   BRIANS DOTFILES'
+  printf "${BLUE}%s\n" "Hooray! Brian's Dotfiles have been updated and/or are at the current version."
 else
-  printf '\033[0;31m%s\033[0m\n' 'There was an error updating. Try again later?'
+  printf "${RED}%s${NORMAL}\n" 'There was an error updating. Try again later?'
 fi
-
